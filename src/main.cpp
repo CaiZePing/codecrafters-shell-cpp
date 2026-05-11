af909daba52f768279623031d6c11337017c627a
CaiZePing <610137894@qq.com>
2026-05-11 22:15:00 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -34,36 +34,38 @@ int main() {
         std::cout << word << " ";
       std::cout << std::endl;
     } else if (command == "type") { // 如果指令为 type
-      bool found = false; // 是否为内部命令
+      bool found = false; // 是否找到
       std::string builtin[3] = {"echo", "type", "exit"}; // 当前的内部命令
       std::string command_to_know; // type 后面接着的命令
       ss >> command_to_know;
       // 将当前的内部命令遍历一遍，查找一下是不是内部命令
-      for (int i = 0; i <= builtin->length(); i++) {
-	if (builtin[i] == command_to_know) {
-	  std::cout << command_to_know << " is a shell builtin\n";
-	  found = true;
-	  break;
-	}
+      for (int i = 0; i < sizeof(builtin)/sizeof(builtin[0]); i++) {
+	      if (builtin[i] == command_to_know) {
+	        std::cout << command_to_know << " is a shell builtin\n";
+	        found = true;
+	        break;
+	      }
       }
       // 没有找到内部命令，看是不是可执行文件
       if (!found) {
-	// 获取环境变量
+	      // 获取环境变量
         std::string path_env = std::getenv("PATH");
-	std::stringstream ss_path(path_env);
+	      std::stringstream ss_path(path_env);
         std::string path;
-	// 拆分环境变量
-	while(std::getline(ss_path,path,PATH_LIST_SEPARATOR)) {
+	      // 拆分环境变量
+	      while(std::getline(ss_path,path,PATH_LIST_SEPARATOR)) {
+          // 拼接路径名 + 文件名
           std::string full_path = path + '/' + command_to_know;
-	  if(access(full_path.c_str(),X_OK) == 0) {
-	    std::cout << command_to_know << " is " << full_path << std::endl;
-	    found = true;
-	    break;
+          // 查看是不是可执行文件
+          if(access(full_path.c_str(),X_OK) == 0) {
+            std::cout << command_to_know << " is " << full_path << std::endl;
+            found = true;
+            break;
           }
-	}  
+	      }
       }
       if (!found) {
-	std::cout << command_to_know << ": not found" << std::endl;
+	      std::cout << command_to_know << ": not found" << std::endl;
       }
     } else {
       std::cout << command << ": command not found" << std::endl;
