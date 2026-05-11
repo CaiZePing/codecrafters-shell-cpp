543b922ee8dfa267a38f36448e3c5b809ee9aa81
CaiZePing <610137894@qq.com>
2026-05-11 20:53:02 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,17 @@ int main() {
   while (true) {
     // TODO: Uncomment the code below to pass the first stage
     std::cout << "$ ";
+    // get line
     std:getline(std::cin, command);
+    // check for exit
     if(command == "exit") {
       break;
     }
+    // check for echo
     if(command.substr(0, 5) == "echo ") {
       std::cout << command.substr(5) << std::endl;
+    } else if(command.substr(0,5) == "type ") {
+      std::cout << command.substr(5) << " is a shell builtin" << std::endl;
     } else {
       std::cout << command << ": command not found" <<std::endl;
     }
