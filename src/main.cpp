6cad5f25fb5c5b7656772e3fd53d52e2b15177b0
CaiZePing <610137894@qq.com>
2026-05-11 22:30:37 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,20 @@
 #include <sstream>
 #include <string>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
+#include <cstring>
+
 
 #ifdef _WIN32
 constexpr char PATH_LIST_SEPARATOR = ';';
 #else
 constexpr char PATH_LIST_SEPARATOR = ':';
 #endif
+// 查看是不是可执行文件
+bool is_executable(const std::string& path);
+// 运行可执行文件
+bool run_executable(const std::string& path, char* const argv[]);
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -16,6 +24,7 @@ int main() {
   std::cerr << std::unitbuf;
   std::string line;
   std::string command;
+  bool is_executable_found = false; // 是不是可执行文件
 
   while (true) {
     // TODO: Uncomment the code below to pass the first stage
@@ -68,7 +77,52 @@ int main() {
 	      std::cout << command_to_know << ": not found" << std::endl;
       }
     } else {
+      // 获取环境变量
+      std::string path_env = std::getenv("PATH");
+      std::stringstream ss_path(path_env);
+      std::string path;
+      char *argv[] = {const_cast<char*>(command.c_str()), nullptr};
+      // 拆分环境变量
+      while(std::getline(ss_path,path,PATH_LIST_SEPARATOR)) {
+        // 拼接路径名 + 文件名
+        std::string full_path = path + '/' + command;
+        // 查看是不是可执行文件
+        if(access(full_path.c_str(),X_OK) == 0) {
+          is_executable_found = true;
+          run_executable(full_path, argv);
+          break;
+        }
+      }
+    }
+    if (!is_executable_found) {
       std::cout << command << ": command not found" << std::endl;
-    }    
+    }
   }
+}
+// 查看是不是可执行文件
+bool is_executable(const std::string& path) {
+  return access(path.c_str(), X_OK) == 0;
+}
+
+// 运行可执行文件
+bool run_executable(const std::string& path, char* const argv[])
+{
+  // 开一个新的进程
+  pid_t pid = fork();
+  if (pid == -1) {
+    std::cerr << "Failed to fork process\n";
+    return false;
+  } else if (pid == 0) {
+    // 在子进程中执行可执行文件
+    execv(path.c_str(), argv);
+    // 如果 execv 返回，说明执行失败
+    std::cerr << "Failed to execute " << path << "\n";
+    exit(1);
+  } else {
+    // 在父进程中等待子进程结束
+    int status;
+    waitpid(pid, &status, 0);
+    return WIFEXITED(status) && WEXITSTATUS(status) == 0;
+  }
+  return 0;
 }
