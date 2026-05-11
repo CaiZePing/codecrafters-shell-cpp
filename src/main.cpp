6f092475057c5fdb413b3713d8a21ec321dad771
CaiZePing <610137894@qq.com>
2026-05-11 21:52:52 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,71 @@
 #include <iostream>
+#include <ranges>
+#include <sstream>
 #include <string>
+#include <unistd.h>
+
+#ifdef _WIN32
+constexpr char PATH_LIST_SEPARATOR = ';';
+#else
+constexpr char PATH_LIST_SEPARATOR = ':';
+#endif
 
 int main() {
-  std::string command;
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  std::string line;
+  std::string command;
 
   while (true) {
     // TODO: Uncomment the code below to pass the first stage
     std::cout << "$ ";
     // get line
-    std:getline(std::cin, command);
+    std::getline(std::cin, line);
+    // parse command
+    std::stringstream ss(line);
+    ss >> command;
     // check for exit
     if(command == "exit") {
       break;
-    }
-    // check for echo
-    if(command.substr(0, 5) == "echo ") {
-      std::cout << command.substr(5) << std::endl;
-    } else if(command.substr(0,5) == "type ") {
-      std::string cmd = command.substr(5);
-      if(cmd == "echo" || cmd == "type" || cmd == "exit") {
-        std::cout << command.substr(5) << " is a shell builtin" << std::endl;
-      } else {
-        std::cout << command.substr(5) << " not found" << std::endl;
+    } else if(command == "echo") { // 如果指令为 echo
+      std::string word;
+      while(ss >> word) 
+        std::cout << word << " ";
+      std::cout << std::endl;
+    } else if (command == "type") { // 如果指令为 type
+      bool found = false; // 是否为内部命令
+      std::string builtin[3] = {"echo", "type", "exit"}; // 当前的内部命令
+      std::string command_to_know; // type 后面接着的命令
+      ss >> command_to_know;
+      // 将当前的内部命令遍历一遍，查找一下是不是内部命令
+      for (int i = 0; i <= builtin->length(); i++) {
+	if (builtin[i] == command_to_know) {
+	  std::cout << command_to_know << " is a shell builtin\n";
+	  found = true;
+	}
+	// 没有找到内部命令，看是不是可执行文件
+	if (!found) {
+	  // 获取环境变量
+	  std::string path_env = std::getenv("PATH");
+	  std::stringstream ss_path(path_env);
+	  std::string path;
+	  // 拆分环境变量
+	  while(std::getline(ss_path,path,PATH_LIST_SEPARATOR)) {
+	    std::string full_path = path + '/' + command_to_know;
+	    if(access(full_path.c_str(),X_OK) == 0) {
+	      std::cout << command_to_know << " is " << full_path << std::endl;
+	      found = true;
+	      break;
+	    }
+	  }  
+	}
+	if (!found) {
+	  std::cout << command_to_know << ": not found" << std::endl;
+	}
       }
     } else {
-      std::cout << command << ": command not found" <<std::endl;
-    }
+      std::cout << command << ": command not found" << std::endl;
+    }    
   }
 }
