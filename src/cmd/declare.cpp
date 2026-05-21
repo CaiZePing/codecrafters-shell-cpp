90a7229ed40954c9b88b6f940d6f2f1345483568
CaiZePing <610137894@qq.com>
2026-05-21 18:04:16 +0800

    codecrafters submit [skip ci]

--- src/cmd/declare.cpp
+++ src/cmd/declare.cpp
@@ -14,14 +14,21 @@ void declare(const std::vector<std::string>& parsed) {
             }
         }
     } else if (parsed.size() == 2) {
-        // 它以字母或下划线开头，其余部分可以使用字母、数字和下划线
-        if (!std::isalpha(parsed[1][0]) && parsed[1][0] != '_') {
-            std::cerr << "declare: `" << parsed[1] << "\': not a valid identifier" << std::endl;
-            return;
-        }
         size_t pos = parsed[1].find('=');
         if (pos != std::string::npos) {
-            variables[parsed[1].substr(0, pos)] = parsed[1].substr(pos + 1);
+            std::string key = parsed[1].substr(0, pos);
+            // key 它以字母或下划线开头，其余部分可以使用字母、数字和下划线
+            if (!std::isalpha(key[0]) && key[0] != '_') {
+                std::cerr << "declare: `" << key << "\': not a valid identifier" << std::endl;
+                return;
+            }
+            for (const char c : key) {
+                if (!std::isalnum(c) && c != '_') {
+                    std::cerr << "declare: `" << key << "\': not a valid identifier" << std::endl;
+                    return;
+                }
+            }
+            variables[key] = parsed[1].substr(pos + 1);
         }
     }
 }
