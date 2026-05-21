a0dce18ffafac4641e616b4ce3ebc9a2a00ecd4c
CaiZePing <610137894@qq.com>
2026-05-21 16:27:55 +0800

    codecrafters submit [skip ci]

--- src/cmd/history.cpp
+++ src/cmd/history.cpp
@@ -8,18 +8,24 @@ namespace cmd {
 std::vector<std::string> HISTORY{};
 void history(const std::vector<std::string>& parsed) {
   // 将 parsed 中的命令解析为整数
-  int index = 0;
-  if (!parsed.empty()) {
-    try {
-      index = std::stoi(parsed[parsed.size() - 1]);
-    } catch (const std::exception&) {
-      std::cerr << "Invalid history index" << std::endl;
-      return;
+  if (parsed.size() > 1) {
+    int index = 0;
+    if (!parsed.empty()) {
+      try {
+        index = std::stoi(parsed[parsed.size() - 1]);
+      } catch (const std::exception&) {
+        std::cerr << "Invalid history index" << std::endl;
+        return;
+      }
     }
-  }
 
-  for (int i = HISTORY.size() - index; i < HISTORY.size(); i++) {
-    std::cout << std::format("    {:<2d} {}", i + 1, HISTORY[i]) << std::endl;
+    for (int i = HISTORY.size() - index; i < HISTORY.size(); i++) {
+      std::cout << std::format("    {:<2d} {}", i + 1, HISTORY[i]) << std::endl;
+    }
+  } else {
+    for (size_t i = 0; i < HISTORY.size(); i++) {
+      std::cout << std::format("    {:<2d} {}", i + 1, HISTORY[i]) << std::endl;
+    }
   }
 }
 
