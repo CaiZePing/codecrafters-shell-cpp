1c7c53af0a179d420d19d8493f1dbdcee9b84cbb
CaiZePing <610137894@qq.com>
2026-05-21 17:00:39 +0800

    codecrafters submit [skip ci]

--- src/cmd/history.hpp
+++ src/cmd/history.hpp
@@ -9,4 +9,5 @@ void history(const std::vector<std::string>& parsed);
 void add_to_history(const std::string& command);
 void writeFileToHistory(const std::string& filename);
 void writeFileFromHistory(const std::string& filename);
+void writeFileAppFromHistory(const std::string& filename);
 }
